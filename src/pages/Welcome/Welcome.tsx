1b941f8c484424b013a97523f90e7082d14fde7f
Tymoteusz Lossy <tymek.lossy@gmail.com>
2022-11-08 11:01:11 +0100

    Background-video has been added.

--- src/pages/Welcome/Welcome.tsx
+++ src/pages/Welcome/Welcome.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, {useEffect, useRef} from "react";
+
+import './Welcome.css';
 
 export const Welcome = () => {
+    const vidRef = useRef<HTMLVideoElement | null>(null);
+
+    useEffect(() => {
+        vidRef.current?.play();
+    }, []);
+
     return(
-        <div className="welcome">Welcome</div>
+        <div className="app__welcome">
+            <video
+                src={require('../../assets/video.mp4')}
+                ref={vidRef}
+                loop
+                controls={false}
+                muted
+                className="app__welcome-video"
+            />
+        </div>
     );
 }
