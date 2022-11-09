b4177b896be28f5b3afe2034643ef6ced7ac74ac
Tymoteusz Lossy <tymek.lossy@gmail.com>
2022-11-09 11:35:04 +0100

    Component Spinner has been created.

--- src/components/TableProductsPage/TableProductsPage.tsx
+++ src/components/TableProductsPage/TableProductsPage.tsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import {AllProductsResponse} from 'types';
+import {axiosData} from "../../utils/axiosData";
 
 import './TableProductsPage.css';
+import {Spinner} from "../Spinner/Spinner";
 
 export const TableProductsPage = () => {
+    const [products, setProducts] = useState<AllProductsResponse>([]);
+
+    useEffect(() => {
+        (async () => {
+            const response = await axiosData.get('/product/allProducts');
+            const data: AllProductsResponse = response.data;
+            setProducts(data);
+        })();
+    }, []);
+
     return(
-        <div className="app__products-table section__padding"></div>
+        <div className="app__products-table section__padding">
+            {products.length === 0
+            ? <Spinner/>
+            : <table></table>}
+        </div>
     )
 }
