449e2453e6690b07c6de75fdff71b784a82f08af
Tymoteusz Lossy <tymek.lossy@gmail.com>
2022-11-09 11:04:09 +0100

    Component TableProductsPage has been created.

---
+++ src/components/TableProductsPage/TableProductsPage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import './TableProductsPage.css';
+
+export const TableProductsPage = () => {
+    return(
+        <div className="app__products-table section__padding"></div>
+    )
+}
